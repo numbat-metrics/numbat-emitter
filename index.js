9143c017b38d6db54ac79d65d073aaaeb79636ff
C J Silverio <ceej@npmjs.com>
2014-05-17 11:19:17 -0700

    Insert code here.

---
+++ index.js
@@ -0,0 +1,16 @@
+var
+    _      = require('lodash'),
+    assert = require('assert'),
+    util   = require('util')
+    ;
+
+var Emitter = module.exports = function Emitter(opts)
+{
+    // insert code here
+};
+
+
+Emitter.createClient = function createClient(opts)
+{
+    return new Emitter(opts);
+};
---
+++ test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var
+    lab         = require('lab'),
+    describe    = lab.describe,
+    it          = lab.it,
+    demand      = require('must')
+    ;
+
+describe('numbat-emitter', function()
+{
+    it('should have tests');
+}
