a77ee74905493a353bd37fc739329d00a37fb44b
C J Silverio <ceej@npmjs.com>
2015-02-04 10:34:13 -0800

    Updated lab & other modules. Tests now pass on io.js.

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var
-    lab        = require('lab'),
+    Lab        = require('lab'),
+    lab        = exports.lab = Lab.script(),
     describe   = lab.describe,
     it         = lab.it,
     demand     = require('must'),
